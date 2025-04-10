12e80e67c1b0097feff8d3bcb5822abae1323ba6
nikhitavys <91998620+nikhitavys@users.noreply.github.com>
2025-04-10 15:41:15 -0400

    changed file path

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from sklearn.preprocessing import LabelEncoder
 
 # load the new Excel workbook
-file_path = "/Users/nikhitavysyaraju/Downloads/INFSC 1740/Cleaned_Updated_Marketing_Data.xlsx"
+file_path = "Cleaned_Updated_Marketing_Data.xlsx"
 xls = pd.ExcelFile(file_path)
 
 # ANALYSIS FOR: What is the engagement of longer vs shorter subject lines?
